a3caaa07e2d98c8975d4c9b79f6250c240429636
AdamBalski <adambalski1@gmail.com>
2020-05-11 22:39:35 +0200

    Updated

--- js/main.js
+++ js/main.js
@@ -2,3 +2,11 @@ function scrollUp() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function makeScrollUpAppear() {
+    var elements = document.getElementsByClassName("navbar-scroll-up");
+
+    for(i = 0; i < elements.length; i++) {
+        elements[i].style.display = "flex";
+    }
+}
